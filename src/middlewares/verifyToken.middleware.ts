1951a39f81140efa6f8bb7e9dd6b16f2ecfae076
Josiel Luz <josiel9511791722@gmail.com>
2023-04-17 10:28:21 -0300

    feat: add verify tokem middleware

---
+++ src/middlewares/verifyToken.middleware.ts
@@ -0,0 +1,38 @@
+import { Request, Response, NextFunction } from "express";
+import Jwt from "jsonwebtoken";
+import "dotenv/config";
+import { AppError } from "../errors/AppError";
+
+const verifyTokenMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    let token = req.headers.authorization;
+
+    if (!token) {
+      throw new AppError("Invalid token", 401);
+    }
+
+    token = token.split(" ")[1];
+
+    Jwt.verify(token, `${process.env.SECRET_KEY}`, (error, decoded: any) => {
+      if (error) {
+        throw new AppError(error.message, 401);
+      }
+
+      req.user = {
+        id: decoded.sub,
+      };
+
+      return next();
+    });
+  } catch (error: any) {
+    return res.status(error.statusCode).json({
+      message: error.message,
+    });
+  }
+};
+
+export default verifyTokenMiddleware;
